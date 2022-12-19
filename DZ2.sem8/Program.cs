e9f26fd72a5bd18bfcc22731dbc01e175e86e551
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-19 13:21:00 +0400

    commit DZ2

---
+++ DZ2.sem8/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку
+// с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей
+// суммой элементов: 1 строка
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j], 1}\t");
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(1, 11);
+}
+
+int SumLineEl(int[,] array, int i)
+{
+    int SumLine = array[i, 0];
+    for (int j =1; j < array.GetLength(1); j++)
+    {
+        SumLine += array[i, j];
+    }
+    return SumLine;
+}
+
+Console.WriteLine("Введите количество строк двумерного массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов двумерного массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
+int minSumLine = 0;
+int sumLine = SumLineEl(array, 0);
+for(int i = 1; i < array.GetLength(0); i++)
+{
+    int tempSumLine = SumLineEl(array, i);
+    if(sumLine > tempSumLine)
+    {
+        sumLine = tempSumLine;
+        minSumLine = i;
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"\n{minSumLine + 1}  строка с наименьшей суммой  элементов ({sumLine})");
